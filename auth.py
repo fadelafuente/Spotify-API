2328a114a3250d3f1dd6e27f8f32bb0635675c7e
fadelafuente <fdelafuente242@gmail.com>
2023-05-10 13:50:09 -0500

    added search function and made minor changes

--- auth.py
+++ auth.py
@@ -1,18 +1,17 @@
-import requests
 import base64
 import datetime
 import os
+import requests
 from dotenv import load_dotenv
+from urllib.parse import urlencode
 
 load_dotenv()
 
+# For testing purposes, will remove later 
 client_id = os.environ.get("client_id")
 client_secret = os.environ.get("client_secret")
 redirect_uri = os.environ.get("redirect_uri")
 token_url = os.environ.get("token_url")
-
-# lookup for a token
-# token is for future requests
 
 class SpotifyAPI(object):
     access_token = None
@@ -21,6 +20,7 @@ class SpotifyAPI(object):
     client_id = None
     client_secret = None
     token_url = None
+    base_url = "https://api.spotify.com"
 
     def __init__(self, client_id, client_secret, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -58,11 +58,10 @@ class SpotifyAPI(object):
         token_headers = self.get_token_headers()
 
         response = requests.post(token_url, data=token_data, headers=token_headers)
+        data = response.json()
 
         if response.status_code not in range(200, 299):
-            return False
-        
-        data = response.json()
+            raise Exception(f"Could not authenticate client. Error: {data}")
 
         now = datetime.datetime.now()
         access_token = data["access_token"]
@@ -73,7 +72,58 @@ class SpotifyAPI(object):
         self.access_expired = expires < now
         return True
     
-client = SpotifyAPI(client_id=client_id, client_secret=client_secret)
-auth = client.perform_auth()
+    def get_access_token(self):     
+        access_token = self.access_token
+        expires = self.access_token_expires
+        now = datetime.datetime.now()
 
-print(client.access_token)
+        if access_token == None or expires < now:
+            self.perform_auth()
+            return self.get_access_token()
+        
+        return access_token
+    
+    def get_access_headers(self):
+        access_token = self.get_access_token()
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+        return headers
+    
+    def get_response(self, id, resource_type="albums", version="v1"):
+        endpoint = f"{self.base_url}/{version}/{resource_type}/{id}"
+        headers = self.get_access_headers()
+        response = requests.get(endpoint, headers=headers)
+
+        if response.status_code not in range(200, 299):
+            return {}   
+        return response.json()
+
+    def search(self, query, search_type='artist'):
+        endpoint = f"{self.base_url}/v1/search"
+        headers = self.get_access_headers()
+
+        data = urlencode({"q": query, "type": search_type.lower()})
+
+        lookup_url = f"{endpoint}?{data}"
+        response = requests.get(lookup_url, headers=headers)
+        data = response.json()
+
+        if response.status_code not in range(200, 299):
+            return {}
+                
+        return data
+    
+    def get_albums(self, _id=None):
+        return self.get_response(_id, resource_type="albums")
+    
+    def get_artists(self, _id=None):
+        return self.get_response(_id, resource_type="artists")
+
+client = SpotifyAPI(client_id=client_id, client_secret=client_secret)
+
+#s = client.search("Time", search_type="track")
+
+s = client.get_albums("7fe4Mem3wWgY6zkTFuKUI9")
+
+print(s)
